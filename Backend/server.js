d0a6ccecc2bdc25c2a09460a095e9d104f07dac9
user.name like : 'Adit-yah <user.eamil like : 'adityachimaniya12@gmail.com>
2025-10-23 16:35:27 +0530

    fix bugs

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,13 @@
 import app from './src/app.js'
 import { createServer} from 'http'
 import {setUpSocket} from './src/socket.io/socket.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const httpServer = createServer(app)
 
 setUpSocket(httpServer)
 
-httpServer.listen(3000 , ()=>console.log('server is running on port no 3000'))
+const PORT = process.env.PORT || 3000
+
+httpServer.listen( PORT , ()=>console.log('server is running '))
--- Backend/src/socket.io/socket.js
+++ Backend/src/socket.io/socket.js
@@ -1,4 +1,3 @@
-import { disconnect } from "process";
 import { Server } from "socket.io";
 
 export async function setUpSocket (httpServer ) {
@@ -9,34 +8,29 @@ export async function setUpSocket (httpServer ) {
     //WHEN USER CONNECT
     io.on("connection" , (socket)=>{
 
-        console.log('An user is connected to socket' , socket.id)
-
         // FOR GROUP CHAT
         socket.on('join-group' , ({ groupId , username})=>{
             socket.join(groupId)
-            console.log(username + ' join the room')
             //NOTIFY WHO'S JOIN
             socket.to(groupId).emit('notify' , username )
         })
 
-        //MANAGING IF USER NOT IN ROOM THEN ADDING THEM
-        
         //NOTIFY WHO'S TYPING
         socket.on('typing' , ({groupId ,username})=>{
-            console.log(username + ' is typing...')
             socket.to(groupId).emit('typing' , username)
+        })
+
+        socket.on('stopTyping' , ({groupId , username})=>{
+            socket.to(groupId).emit('stopTyping' , username)
         })
 
         //MENAGE MESSAGES SHARING IN GROUP
         socket.on('message' , ( data)=>{
-
-            console.log(data)
             socket.to(data.groupId).emit('message' , data )
         })
 
         //WHEN USER DISCONNECT
         socket.on('disconnect', ()=>{
-            console.log('An user is disconnected to socket' )
         })
     })
 }
